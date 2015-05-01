52545a0f692a61b6192b64d19bf709dab1e9a183
Manasi Kulkarni <mkulkarn@thoughtworks.com>
2015-05-01 10:18:02 +0100

    Addind ssh config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 sudo sed -i "s/GO_SERVER=127.0.0.1/GO_SERVER=${GO_SERVER_IP}/g" /etc/default/go-agent
 ssh-keygen -N "" -f /var/go/.ssh/id_rsa
+echo -e "Host * \n   StrictHostKeyChecking no" > $HOME/.ssh/config
 sudo service docker start
 sudo service go-agent start
